65e4336dede12d2cbb928a659181607be8d59197
Vishal Repaka <vrepaka@Vishals-MacBook-Pro.local>
2018-03-29 16:10:51 +1100

    00 Hello World - hurred

--- subjects/00 Hello World/exercise.js
+++ subjects/00 Hello World/exercise.js
@@ -8,7 +8,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 function App() {
-  return <div>Hello world!</div>;
+  return <div>Hurrr world!</div>;
 }
 
 ReactDOM.render(<App />, document.getElementById("app"));
